a8246b825daa2f3397883230b7becc3937378cc3
Javacasm <javacasm@gmail.com>
2019-12-15 19:01:02 +0100

    update

--- camera_test.py
+++ camera_test.py
@@ -5,23 +5,28 @@ import time
 d = digoo.Digoo(config.CAMERA_IP)
 print('starting camera with IP '+config.CAMERA_IP)
 
+d.play_video()
+print('Playing video')
+
+time.sleep(1)
 print('moving to the left')
 d.move_left()
 
-print('moving to the left')
-d.move_left()
+time.sleep(1)
+print('moving to the right')
+d.move_right()
 
+time.sleep(1)
 print('moving up')
 d.move_up()
 
+time.sleep(1)
 print('moving down')
 d.move_down()
 
-d.play_video()
-print('Playing video')
-time.sleep(10)
+time.sleep(5)
 
-d.take_snapshot('image.jpg')
+d.take_snapshot('~/image.jpg')
 
 print('Stop video')
 d.stop_video()
